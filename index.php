6e5bcec3326f4f08324fb23857451e4accd091a7
araynaud <arthur.raynaud@icix.com>
2016-03-29 00:23:06 -0700

    on-scroll-h : hide sidebar

--- index.php
+++ index.php
@@ -98,7 +98,7 @@ if(!$offline)
 <?php echoJsVar("pdjConfig");?>
 </script>
 </head>
-<body class="nomargin bgwhite" ng-class="lc.bodyClasses()" ng-controller="LayoutController as lc">
+<body class="nomargin bgwhite" ng-class="lc.bodyClasses()" ng-controller="LayoutController as lc" on-scroll-h="lc.toggleSidebar(false)">
 	<div id="background" class="nomargin bg hidden-print" ng-style="lc.backgroundImage" ng-if="lc.backgroundImage">
 		<div id="backgroundOverlay" class="stretchH" ng-style="lc.overlay" ng-if="lc.overlay"></div>
 	</div>
